0fab12a51f7dfbed41e2fa5380d3731a3af9354d
Daniel Wright <daniel@godynamo.com>
2013-09-12 15:07:04 -0400

    Scaffolds feature specs for new-customer checkout

---
+++ spec/features/new_customer/checkout_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+# In order to pay for the massive slab of Nebraska beef in his cart
+# As a brand-new customer
+# Homer Simpson can check out
+
+describe "A new customer", type: :feature do
+  context "with a credit card" do
+    it "completes a checkout"
+  end
+
+  context "with PayPal" do
+    it "completes a checkout"
+  end
+
+  context "with store credit" do
+    it "completes a checkout"
+  end
+end
