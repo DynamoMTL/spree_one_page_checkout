a54407c426e921b87d3c691d938c4b5c104a73e6
Daniel Wright <daniel@godynamo.com>
2013-09-16 15:02:54 -0400

    Adds golden-path acceptance spec for new-customer checkout

--- spec/features/new_customer/checkout_spec.rb
+++ spec/features/new_customer/checkout_spec.rb
@@ -4,13 +4,66 @@ require 'spec_helper'
 # As a brand-new customer
 # Homer Simpson can check out
 
-describe "A new customer", type: :feature do
+describe "A new customer", type: :feature, js: true do
+  before(:all) { configure_store }
+
   before do
     sign_up!
   end
 
+  let(:beef_slab) { create(:product) }
+
   context "with a credit card" do
-    it "completes a checkout"
+    it "completes a checkout" do
+      visit spree.product_path(beef_slab)
+
+      # product.add_to_cart
+      click_button 'Add To Cart'
+
+      # cart.checkout
+      click_button 'Checkout'
+
+      # address.supply
+      within '[data-hook=opco-shipping-address]' do
+        fill_in 'Full Name', with: 'Guy Incognito'
+        fill_in 'Address', with: '1234 Fake St.'
+        fill_in 'City', with: 'New York City'
+        select 'New York', from: 'State'
+        select 'United States', from: 'Country'
+        fill_in 'Zip Code', with: '10001'
+        fill_in 'Telephone', with: '555-555-1234'
+      end
+
+      # shipping_method.choose
+      within '[data-hook=opco-delivery-method]' do
+        select delivery_method
+      end
+
+      # credit_card.supply
+      within '[data-hook=opco-payment-method]' do
+        within '[data-hook=opco-card-details]' do
+          fill_in 'Card Number', with: '4111111111111111'
+          select '01', from: 'Expiration Month'
+          select '2015', from: 'Expiration Year'
+          fill_in 'Card Verification Value', with: '123'
+        end
+
+        within '[data-hook=opco-billing-address]' do
+          fill_in 'Full Name', with: 'Guy Incognito'
+          fill_in 'Address', with: '1234 Fake St.'
+          fill_in 'City', with: 'New York City'
+          select 'New York', from: 'State'
+          select 'United States', from: 'Country'
+          fill_in 'Zip Code', with: '10001'
+          fill_in 'Telephone', with: '555-555-1234'
+        end
+      end
+
+      # confirmation.confirm
+      click_on 'Confirm My Order'
+
+      expect(page).to have_content "Your order was processed successfully."
+    end
   end
 
   context "with PayPal" do
---
+++ spec/support/integration_macros.rb
@@ -0,0 +1,11 @@
+module IntegrationMacros
+  def configure_store
+    create(:bogus_payment_method)
+
+    # Require a country to be created so the shipping method creates the Global
+    # Zone with the same country as orders' billing and shipping addresses.
+    create(:country)
+  end
+end
+
+RSpec.configure { |c| c.include IntegrationMacros, type: :feature }
