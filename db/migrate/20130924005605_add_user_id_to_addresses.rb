cc06d13fd858cc6ea12cb86e5a652b7e6156f119
Daniel Wright <daniel@godynamo.com>
2013-10-21 17:03:01 -0400

    Fixes Addresses migration when the user_id column already exists
    Fixes #2

--- db/migrate/20130924005605_add_user_id_to_addresses.rb
+++ db/migrate/20130924005605_add_user_id_to_addresses.rb
@@ -1,7 +1,17 @@
 class AddUserIdToAddresses < ActiveRecord::Migration
   def change
-    change_table Spree::Address.table_name do |t|
+    change_table table_name do |t|
       t.references :user
     end
+  end
+
+  def migrate(direction)
+    super unless direction == :up && column_exists?(table_name, :user_id)
+  end
+
+  private
+
+  def table_name
+    Spree::Address.table_name
   end
 end
