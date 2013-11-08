3c7ff567a4ba3df1276872a39ed75f75f5bb6af5
Daniel Wright <daniel@godynamo.com>
2013-11-08 11:13:05 -0500

    Configures VCR in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,13 @@ Dotenv.load
 require 'factory_girl'
 FactoryGirl.find_definitions
 
+require 'vcr'
+VCR.configure do |vcr|
+  vcr.allow_http_connections_when_no_cassette = true
+  vcr.cassette_library_dir = 'spec/support/cassettes'
+  vcr.hook_into :webmock
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
