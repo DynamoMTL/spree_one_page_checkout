e4e8188a199adf508fc07b36927aa9a79d3e96fd
Ricardo Andrés Bello <ricardoaandres@me.com>
2014-09-08 16:14:16 -0300

    Update directory structure to fit spree-2-3

--- lib/generators/spree_one_page_checkout/install/install_generator.rb
+++ lib/generators/spree_one_page_checkout/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeOnePageCheckout
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_one_page_checkout\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_one_page_checkout\n"
+        append_file 'app/assets/javascripts/frontend/all.js', "//= require frontend/spree_one_page_checkout\n"
+        append_file 'app/assets/javascripts/backend/all.js', "//= require backend/spree_one_page_checkout\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/frontend/all.css', " *= require frontend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/backend/all.css', " *= require backend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
